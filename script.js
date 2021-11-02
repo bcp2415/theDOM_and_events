1644f0fd59d1127ee869ef02dd20626e530ae92f
Brian D. Prince <64213121+bcp2415@users.noreply.github.com>
2021-11-02 15:20:08 -0500

    bringing repo up to date as I get started

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+    button.addEventListener("click", event => {
+        paragraph.innerHTML = "Houston, we have liftoff!"
+    });
     
-}
+    /*missionAbort.addEventListenever("mouseover", event => {
+        missionAbort.style.backgroundColor = "red";
+    });*/
 
 window.addEventListener("load", init);
